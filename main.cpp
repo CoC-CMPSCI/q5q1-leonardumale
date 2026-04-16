ee3f1bcfd93d8ab4025e7014840aed2170471cd2
leonardumale <leonardumale@gmail.com>
2026-04-15 19:03:22 -0700

    Implement letter printing based on user input
    Refactor main function to include user input and nested loops for letter printing.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     int n;
-    // TODO: ask user for input
-
-    // TODO: outer loop for each row
-
-        // TODO: inner loop to print letters for this row
-
-            // TODO: print the letter for this column
-
-        // TODO: print newline after each row
-
-
+    cin >> n;
+    for (int i = 0; i <= n; i++;) {
+        for (int j = 0; j <= n; j++) {
+            char letter = 'A' + j;
+            cout << letter;
+                if (j < i){
+                    cout << " ";
+                }
+        }
+        cout << endl;
+    }
     return 0;
 }
+
